e92c021f3caaa1c5b7a96354f55c2991e47dca37
amreltohamy <59233815+amreltohamy@users.noreply.github.com>
2020-12-12 00:42:18 +0200

    done and accepted

---
+++ A Very Big Sum.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+
+func aVeryBigSum(ar: [Int]) -> Int {
+    var sum = 0
+    for i in ar {
+        sum += i
+    }
+return sum
+}
--- Day 2- Operators.playground/Contents.swift
+++ Day 2- Operators.playground/Contents.swift
@@ -3,7 +3,7 @@ import Cocoa
 func solve(meal_cost: Double, tip_percent: Int, tax_percent: Int) -> Void {
 
     let totalCost = round(meal_cost + ( Double(tip_percent+tax_percent)*meal_cost)/100)
-    print(Int(totalCost))
+    print(Int(totalCost-140))
 
 }
-solve(meal_cost: 95, tip_percent: 15, tax_percent: 7)
+solve(meal_cost: 140, tip_percent: 30, tax_percent: 0)
